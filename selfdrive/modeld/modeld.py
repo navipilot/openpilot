38ad12db66b700f0056a8860d986f8de0e2c85a5
ajouatom <ajouatom@gmail.com>
2026-03-17 10:09:36 +0900

    fix.. calibrate..

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -362,8 +362,13 @@ def main(demo=False):
     #lateral_control_params = np.array([v_ego, lat_delay], dtype=np.float32)
     if sm.updated["liveCalibration"] and sm.seen['roadCameraState'] and sm.seen['deviceState']:
       device_from_calib_euler = np.array(sm["liveCalibration"].rpyCalib, dtype=np.float32)
-      if camera_yaw_trim_deg != 0.0:
-        device_from_calib_euler[2] += np.radians(camera_yaw_trim_deg)
+
+      calib_done = sm["liveCalibration"].calStatus == log.LiveCalibrationData.Status.calibrated
+      applied_yaw_trim_deg = camera_yaw_trim_deg if calib_done else 0.0
+
+      if applied_yaw_trim_deg != 0.0:
+        device_from_calib_euler[2] += np.radians(applied_yaw_trim_deg)
+        
       dc = DEVICE_CAMERAS[(str(sm['deviceState'].deviceType), str(sm['roadCameraState'].sensor))]
       model_transform_main = get_warp_matrix(device_from_calib_euler, dc.ecam.intrinsics if main_wide_camera else dc.fcam.intrinsics, False).astype(np.float32)
       model_transform_extra = get_warp_matrix(device_from_calib_euler, dc.ecam.intrinsics, True).astype(np.float32)
