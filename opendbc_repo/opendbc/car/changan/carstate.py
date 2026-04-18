e244cbd46062a7bb3fd1b6c4b43dee7b37621d4b
Navipilot <61354095+navipilot@users.noreply.github.com>
2026-04-18 14:10:32 +0800

    Add files via upload

--- opendbc_repo/opendbc/car/changan/carstate.py
+++ opendbc_repo/opendbc/car/changan/carstate.py
@@ -12,6 +12,7 @@ class CarState(CarStateBase):
     super().__init__(CP)
     can_define = CANDefine(DBC[CP.carFingerprint][Bus.pt])
 
+    # GW_331 (Z6) / GW_338 (Z6_IDD) 均有 TCU_GearForDisplay : 39|4@0+
     if CP.carFingerprint == CAR.CHANGAN_Z6:
       self.shifter_values = can_define.dv.get("GW_331", {}).get("TCU_GearForDisplay", {})
       if not self.shifter_values:
@@ -66,11 +67,12 @@ class CarState(CarStateBase):
       return default
 
     # 车门 / 安全带
-    ret.doorOpen = sig(cp.vl, "GW_28B", ["BCM_DriverDoorStatus", "doorOpen"], 0) == 1
-    ret.seatbeltUnlatched = sig(cp.vl, "GW_50", ["SRS_DriverBuckleSwitchStatus", "seatbeltUnlatched"], 0) == 1
+    ret.doorOpen = sig(cp.vl, "GW_28B", ["BCM_DriverDoorStatus"], 0) == 1
+    ret.seatbeltUnlatched = sig(cp.vl, "GW_50", ["SRS_DriverBuckleSwitchStatus"], 0) == 1
     ret.parkingBrake = False
 
     # 车速
+    # GW_187 / GW_17A: ESP_VehicleSpeed : 36|13@0+ (0.05625,0) 单位 km/h
     if self.CP.carFingerprint == CAR.CHANGAN_Z6_IDD:
       carspd = sig(cp.vl, "GW_17A", ["ESP_VehicleSpeed"], 0)
     else:
@@ -81,16 +83,22 @@ class CarState(CarStateBase):
     ret.vEgoCluster = ret.vEgo
     ret.standstill = abs(ret.vEgoRaw) < 1e-3
 
-    # 油门 / 制动 / 挡位
+    # 油门 / 制动
+    # GW_196: EMS_BrakePedalStatus : 55|2@0+  EMS_RealAccPedal : 7|8@0+
+    # GW_1A6: EMS_BrakePedalStatus : 35|2@0+  EMS_AccPedal     : 47|8@0+
     if self.CP.carFingerprint == CAR.CHANGAN_Z6_IDD:
-      ret.brakePressed = sig(cp.vl, "GW_1A6", ["EMS_BrakePedalStatus", "brakePressed"], 0) != 0
-      ret.gasPressed = sig(cp.vl, "GW_1A6", ["EMS_RealAccPedal", "gasPressed"], sig(cp.vl, "GW_1C6", ["EMS_RealAccPedal"], 0)) != 0
+      ret.brakePressed = sig(cp.vl, "GW_1A6", ["EMS_BrakePedalStatus"], 0) != 0
+      ret.gasPressed = sig(cp.vl, "GW_1A6", ["EMS_AccPedal"],
+                           sig(cp.vl, "GW_1C6", ["EMS_RealAccPedal"], 0)) != 0
     else:
-      ret.brakePressed = sig(cp.vl, "GW_196", ["EMS_BrakePedalStatus", "brakePressed"], 0) != 0
-      ret.gasPressed = sig(cp.vl, "GW_196", ["EMS_RealAccPedal", "gasPressed"], 0) != 0
+      ret.brakePressed = sig(cp.vl, "GW_196", ["EMS_BrakePedalStatus"], 0) != 0
+      ret.gasPressed = sig(cp.vl, "GW_196", ["EMS_RealAccPedal"], 0) != 0
 
+    # 挡位
+    # GW_331 / GW_338: TCU_GearForDisplay : 39|4@0+
     if self.CP.carFingerprint == CAR.CHANGAN_Z6:
-      can_gear = int(sig(cp.vl, "GW_331", ["TCU_GearForDisplay"], sig(cp.vl, "GW_338", ["TCU_GearForDisplay"], 0)))
+      can_gear = int(sig(cp.vl, "GW_331", ["TCU_GearForDisplay"],
+                         sig(cp.vl, "GW_338", ["TCU_GearForDisplay"], 0)))
     else:
       can_gear = int(sig(cp.vl, "GW_338", ["TCU_GearForDisplay"], 0))
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(can_gear, None))
@@ -98,16 +106,19 @@ class CarState(CarStateBase):
     ret.rightBlindspot = False
 
     # 转向灯
-    left_blinker = sig(cp.vl, "GW_28B", ["BCM_TurnIndicatorLeft", "leftBlinker"], 0) == 1
-    right_raw = sig(cp.vl, "GW_28B", ["BCM_TurnIndicatorRight", "rightBlinker"], 0)
+    # GW_28B: BCM_TurnIndicatorLeft / BCM_TurnIndicatorRight
+    left_blinker = sig(cp.vl, "GW_28B", ["BCM_TurnIndicatorLeft"], 0) == 1
+    right_raw = sig(cp.vl, "GW_28B", ["BCM_TurnIndicatorRight"], 0)
     ret.leftBlinker, ret.rightBlinker = self.update_blinker_from_stalk(200, left_blinker, right_raw in (1, 2))
 
     # 转向角 / 扭矩
+    # GW_180: SAS_SteeringAngle : 7|16@0-  SAS_SteeringAngleSpeed : 23|8@0-
+    # GW_17E: EPS_MeasuredTorsionBarTorque : 7|12@0+  EPS_ActualTorsionBarTorq (GW_170)
     ret.steeringAngleOffsetDeg = 0
-    ret.steeringAngleDeg = sig(cp.vl, "GW_180", ["SAS_SteeringAngle", "steeringAngleDeg"], 0)
-    ret.steeringRateDeg = cp.vl["GW_180"]["SAS_SteeringAngleSpeed"]
-    ret.steeringTorque = cp.vl["GW_17E"]["EPS_MeasuredTorsionBarTorque"]
-    ret.steeringTorqueEps = cp.vl["GW_170"]["EPS_ActualTorsionBarTorq"] * self.eps_torque_scale
+    ret.steeringAngleDeg = sig(cp.vl, "GW_180", ["SAS_SteeringAngle"], 0)
+    ret.steeringRateDeg = sig(cp.vl, "GW_180", ["SAS_SteeringAngleSpeed"], 0)
+    ret.steeringTorque = sig(cp.vl, "GW_17E", ["EPS_MeasuredTorsionBarTorque"], 0)
+    ret.steeringTorqueEps = sig(cp.vl, "GW_170", ["EPS_ActualTorsionBarTorq"], 0) * self.eps_torque_scale
 
     if self.steeringPressed:
       if abs(ret.steeringTorque) < self.steeringPressedMin and abs(ret.steeringAngleDeg) < 90:
@@ -118,22 +129,29 @@ class CarState(CarStateBase):
     ret.steeringPressed = self.steeringPressed
 
     # 原车 ACC 状态透传
-    acc_mode = cp_cam.vl["GW_31A"]["ACC_IACCHWAMode"] if "ACC_IACCHWAMode" in cp_cam.vl["GW_31A"] else 0
+    # GW_244: ACC_ACCMode : 54|3@0+  (2=Standby, 3=Active, 7=Fault)
+    # GW_31A: ACC_IACCHWAMode : 135|3@0+  (3=Active)
+    acc_mode_244 = sig(cp_cam.vl, "GW_244", ["ACC_ACCMode"], 0)
+    acc_mode_31a = sig(cp_cam.vl, "GW_31A", ["ACC_IACCHWAMode"], 0)
     ret.cruiseState.available = True
-    ret.cruiseState.enabled = acc_mode == 3
-    ret.cruiseState.speed = cp_cam.vl["GW_307"]["vCruise"] * CV.KPH_TO_MS if "vCruise" in cp_cam.vl["GW_307"] else 0
+    ret.cruiseState.enabled = (acc_mode_244 == 3)
+
+    # GW_307: ACC_SetSpeed : 6|8@0+ (1,0) 单位 km/h（注意：DBC信号名是 ACC_SetSpeed，不是 vCruise）
+    ret.cruiseState.speed = sig(cp_cam.vl, "GW_307", ["ACC_SetSpeed"], 0) * CV.KPH_TO_MS
     ret.cruiseState.standstill = ret.standstill
 
     # 故障检测
-    ret.accFaulted = cp_cam.vl["GW_244"]["ACC_ACCMode"] == 7 or cp_cam.vl["GW_31A"]["ACC_IACCHWAMode"] == 7
+    ret.accFaulted = (acc_mode_244 == 7) or (acc_mode_31a == 7)
     eps_failed = sig(cp.vl, "GW_24F", ["EPS_EPSFailed"], 0) != 0
+    # GW_17E: EPS_LatCtrlAvailabilityStatus : 53|2@0+ (1=OK, 2=Unavailable)
     lat_ctrl_unavailable = sig(cp.vl, "GW_17E", ["EPS_LatCtrlAvailabilityStatus"], 1) == 2
     ret.steerFaultTemporary = eps_failed or lat_ctrl_unavailable
     ret.steerFaultPermanent = False
 
     # 原车 ADAS 状态透传
-    ret.stockFcw = cp_cam.vl["GW_244"]["ACC_FCWPreWarning"] == 1
-    ret.stockAeb = cp_cam.vl["GW_244"]["ACC_AEBCtrlType"] > 0
+    # GW_244: ACC_FCWPreWarning : 91|1@0+  ACC_AEBCtrlType : 104|4@0+
+    ret.stockFcw = sig(cp_cam.vl, "GW_244", ["ACC_FCWPreWarning"], 0) == 1
+    ret.stockAeb = sig(cp_cam.vl, "GW_244", ["ACC_AEBCtrlType"], 0) > 0
     ret.genericToggle = False
 
     # 控制器用信号快照
@@ -144,33 +162,35 @@ class CarState(CarStateBase):
     self.sigs31a = copy.copy(cp_cam.vl["GW_31A"])
 
     # 滚动计数器提取
-    self.counter_244 = cp_cam.vl["GW_244"]["ACC_RollingCounter_24E"]
-    self.counter_1ba = sig(cp_cam.vl, "GW_1BA", ["ACC_RollingCounter_1BA", "Counter_1BA"], 0)
-    self.counter_17e = cp.vl["GW_17E"]["EPS_RollingCounter_17E"]
-    self.counter_307 = sig(cp_cam.vl, "GW_307", ["ACC_RollingCounter_35E", "Counter_35E"], 0)
-    self.counter_31a = sig(cp_cam.vl, "GW_31A", ["ACC_RollingCounter_36D", "Counter_36D"], 0)
+    self.counter_244 = sig(cp_cam.vl, "GW_244", ["ACC_RollingCounter_24E"], 0)
+    self.counter_1ba = sig(cp_cam.vl, "GW_1BA", ["ACC_RollingCounter_1BA"], 0)
+    self.counter_17e = sig(cp.vl, "GW_17E", ["EPS_RollingCounter_17E"], 0)
+    self.counter_307 = sig(cp_cam.vl, "GW_307", ["ACC_RollingCounter_35E"], 0)
+    self.counter_31a = sig(cp_cam.vl, "GW_31A", ["ACC_RollingCounter_36D"], 0)
 
-    # 跟车距离按钮
+    # 跟车距离按钮 GW_307: ACC_DistanceLevel : 22|3@0+
     self.prev_distance_button = self.distance_button
-    self.distance_button = cp_cam.vl["GW_307"]["ACC_DistanceLevel"]
+    self.distance_button = sig(cp_cam.vl, "GW_307", ["ACC_DistanceLevel"], 0)
 
     return ret
 
   @staticmethod
   def get_can_parsers(CP):
     pt_messages = [
-      ("GW_50", 2),
+      ("GW_50",  2),
       ("GW_170", 100),
       ("GW_17E", 100),
       ("GW_180", 100),
       ("GW_24F", 50),
       ("GW_28B", 25),
+      ("GW_28C", 50),
     ]
 
     if CP.carFingerprint == CAR.CHANGAN_Z6:
       pt_messages += [
-        ("GW_331", 10),
-        ("GW_338", 10),
+        # 频率设 0：部分 Z6 车辆 bus0 不广播 GW_331，设为可选避免 CAN_INVALID
+        ("GW_331", 0),
+        ("GW_338", 0),
       ]
     else:
       pt_messages += [
@@ -193,10 +213,10 @@ class CarState(CarStateBase):
       ("GW_1BA", 100),
       ("GW_244", 50),
       ("GW_307", 10),
-      ("GW_31A", 100),
+      ("GW_31A", 10),   # GW_31A ACC_IACCHWAMode 在 byte16，实际更新较慢
     ]
 
     return {
-      Bus.pt: CANParser(DBC[CP.carFingerprint][Bus.pt], pt_messages, 0),
+      Bus.pt:  CANParser(DBC[CP.carFingerprint][Bus.pt], pt_messages, 0),
       Bus.cam: CANParser(DBC[CP.carFingerprint][Bus.pt], cam_messages, 2),
     }
--- opendbc_repo/opendbc/car/changan/changancan.py
+++ opendbc_repo/opendbc/car/changan/changancan.py
@@ -1,390 +1,134 @@
 crc8_tab = [
-  0x0,
-  0x1D,
-  0x3A,
-  0x27,
-  0x74,
-  0x69,
-  0x4E,
-  0x53,
-  0xE8,
-  0xF5,
-  0xD2,
-  0xCF,
-  0x9C,
-  0x81,
-  0xA6,
-  0xBB,
-  0xCD,
-  0xD0,
-  0xF7,
-  0xEA,
-  0xB9,
-  0xA4,
-  0x83,
-  0x9E,
-  0x25,
-  0x38,
-  0x1F,
-  0x2,
-  0x51,
-  0x4C,
-  0x6B,
-  0x76,
-  0x87,
-  0x9A,
-  0xBD,
-  0xA0,
-  0xF3,
-  0xEE,
-  0xC9,
-  0xD4,
-  0x6F,
-  0x72,
-  0x55,
-  0x48,
-  0x1B,
-  0x6,
-  0x21,
-  0x3C,
-  0x4A,
-  0x57,
-  0x70,
-  0x6D,
-  0x3E,
-  0x23,
-  0x4,
-  0x19,
-  0xA2,
-  0xBF,
-  0x98,
-  0x85,
-  0xD6,
-  0xCB,
-  0xEC,
-  0xF1,
-  0x13,
-  0xE,
-  0x29,
-  0x34,
-  0x67,
-  0x7A,
-  0x5D,
-  0x40,
-  0xFB,
-  0xE6,
-  0xC1,
-  0xDC,
-  0x8F,
-  0x92,
-  0xB5,
-  0xA8,
-  0xDE,
-  0xC3,
-  0xE4,
-  0xF9,
-  0xAA,
-  0xB7,
-  0x90,
-  0x8D,
-  0x36,
-  0x2B,
-  0xC,
-  0x11,
-  0x42,
-  0x5F,
-  0x78,
-  0x65,
-  0x94,
-  0x89,
-  0xAE,
-  0xB3,
-  0xE0,
-  0xFD,
-  0xDA,
-  0xC7,
-  0x7C,
-  0x61,
-  0x46,
-  0x5B,
-  0x8,
-  0x15,
-  0x32,
-  0x2F,
-  0x59,
-  0x44,
-  0x63,
-  0x7E,
-  0x2D,
-  0x30,
-  0x17,
-  0xA,
-  0xB1,
-  0xAC,
-  0x8B,
-  0x96,
-  0xC5,
-  0xD8,
-  0xFF,
-  0xE2,
-  0x26,
-  0x3B,
-  0x1C,
-  0x1,
-  0x52,
-  0x4F,
-  0x68,
-  0x75,
-  0xCE,
-  0xD3,
-  0xF4,
-  0xE9,
-  0xBA,
-  0xA7,
-  0x80,
-  0x9D,
-  0xEB,
-  0xF6,
-  0xD1,
-  0xCC,
-  0x9F,
-  0x82,
-  0xA5,
-  0xB8,
-  0x3,
-  0x1E,
-  0x39,
-  0x24,
-  0x77,
-  0x6A,
-  0x4D,
-  0x50,
-  0xA1,
-  0xBC,
-  0x9B,
-  0x86,
-  0xD5,
-  0xC8,
-  0xEF,
-  0xF2,
-  0x49,
-  0x54,
-  0x73,
-  0x6E,
-  0x3D,
-  0x20,
-  0x7,
-  0x1A,
-  0x6C,
-  0x71,
-  0x56,
-  0x4B,
-  0x18,
-  0x5,
-  0x22,
-  0x3F,
-  0x84,
-  0x99,
-  0xBE,
-  0xA3,
-  0xF0,
-  0xED,
-  0xCA,
-  0xD7,
-  0x35,
-  0x28,
-  0xF,
-  0x12,
-  0x41,
-  0x5C,
-  0x7B,
-  0x66,
-  0xDD,
-  0xC0,
-  0xE7,
-  0xFA,
-  0xA9,
-  0xB4,
-  0x93,
-  0x8E,
-  0xF8,
-  0xE5,
-  0xC2,
-  0xDF,
-  0x8C,
-  0x91,
-  0xB6,
-  0xAB,
-  0x10,
-  0xD,
-  0x2A,
-  0x37,
-  0x64,
-  0x79,
-  0x5E,
-  0x43,
-  0xB2,
-  0xAF,
-  0x88,
-  0x95,
-  0xC6,
-  0xDB,
-  0xFC,
-  0xE1,
-  0x5A,
-  0x47,
-  0x60,
-  0x7D,
-  0x2E,
-  0x33,
-  0x14,
-  0x9,
-  0x7F,
-  0x62,
-  0x45,
-  0x58,
-  0xB,
-  0x16,
-  0x31,
-  0x2C,
-  0x97,
-  0x8A,
-  0xAD,
-  0xB0,
-  0xE3,
-  0xFE,
-  0xD9,
-  0xC4,
+  0x0, 0x1D, 0x3A, 0x27, 0x74, 0x69, 0x4E, 0x53, 0xE8, 0xF5, 0xD2, 0xCF, 0x9C, 0x81, 0xA6, 0xBB,
+  0xCD, 0xD0, 0xF7, 0xEA, 0xB9, 0xA4, 0x83, 0x9E, 0x25, 0x38, 0x1F, 0x2, 0x51, 0x4C, 0x6B, 0x76,
+  0x87, 0x9A, 0xBD, 0xA0, 0xF3, 0xEE, 0xC9, 0xD4, 0x6F, 0x72, 0x55, 0x48, 0x1B, 0x6, 0x21, 0x3C,
+  0x4A, 0x57, 0x70, 0x6D, 0x3E, 0x23, 0x4, 0x19, 0xA2, 0xBF, 0x98, 0x85, 0xD6, 0xCB, 0xEC, 0xF1,
+  0x13, 0xE, 0x29, 0x34, 0x67, 0x7A, 0x5D, 0x40, 0xFB, 0xE6, 0xC1, 0xDC, 0x8F, 0x92, 0xB5, 0xA8,
+  0xDE, 0xC3, 0xE4, 0xF9, 0xAA, 0xB7, 0x90, 0x8D, 0x36, 0x2B, 0xC, 0x11, 0x42, 0x5F, 0x78, 0x65,
+  0x94, 0x89, 0xAE, 0xB3, 0xE0, 0xFD, 0xDA, 0xC7, 0x7C, 0x61, 0x46, 0x5B, 0x8, 0x15, 0x32, 0x2F,
+  0x59, 0x44, 0x63, 0x7E, 0x2D, 0x30, 0x17, 0xA, 0xB1, 0xAC, 0x8B, 0x96, 0xC5, 0xD8, 0xFF, 0xE2,
+  0x26, 0x3B, 0x1C, 0x1, 0x52, 0x4F, 0x68, 0x75, 0xCE, 0xD3, 0xF4, 0xE9, 0xBA, 0xA7, 0x80, 0x9D,
+  0xEB, 0xF6, 0xD1, 0xCC, 0x9F, 0x82, 0xA5, 0xB8, 0x3, 0x1E, 0x39, 0x24, 0x77, 0x6A, 0x4D, 0x50,
+  0xA1, 0xBC, 0x9B, 0x86, 0xD5, 0xC8, 0xEF, 0xF2, 0x49, 0x54, 0x73, 0x6E, 0x3D, 0x20, 0x7, 0x1A,
+  0x6C, 0x71, 0x56, 0x4B, 0x18, 0x5, 0x22, 0x3F, 0x84, 0x99, 0xBE, 0xA3, 0xF0, 0xED, 0xCA, 0xD7,
+  0x35, 0x28, 0xF, 0x12, 0x41, 0x5C, 0x7B, 0x66, 0xDD, 0xC0, 0xE7, 0xFA, 0xA9, 0xB4, 0x93, 0x8E,
+  0xF8, 0xE5, 0xC2, 0xDF, 0x8C, 0x91, 0xB6, 0xAB, 0x10, 0xD, 0x2A, 0x37, 0x64, 0x79, 0x5E, 0x43,
+  0xB2, 0xAF, 0x88, 0x95, 0xC6, 0xDB, 0xFC, 0xE1, 0x5A, 0x47, 0x60, 0x7D, 0x2E, 0x33, 0x14, 0x9,
+  0x7F, 0x62, 0x45, 0x58, 0xB, 0x16, 0x31, 0x2C, 0x97, 0x8A, 0xAD, 0xB0, 0xE3, 0xFE, 0xD9, 0xC4,
 ]
 
 
 def crc_calculate_crc8(data):
-  crc = 0xFF
+  crc = 0
   for byte in data:
     crc = crc8_tab[crc ^ byte]
-  return crc ^ 0xFF
-
-
-def xor_checksum(data):
-  checksum = 0
-  for byte in data:
-    checksum ^= byte
-  return checksum
+  return crc
 
 
 def create_steering_control(packer, msg, angle, active, counter):
+  """GW_1BA (0x1BA, 32byte): 转向角度指令
+  DBC信号:
+    EPS_AngleCmd          : 17|14@0+ (0.1,-720)
+    EPS_LatCtrlActive     : 35|1@0+
+    ACC_RollingCounter_1BA: 51|4@0+
+    ACC_CRCCheck_1BA      : 63|8@0+  (bytes 0~7 的 CRC)
+  """
   values = {s: msg[s] for s in msg} if msg else {}
-  values.update(
-    {
-      "ACC_SteeringAngleSub_1BA": angle,
-      "ACC_SteeringAngleReq_1BA": active,
-      "Counter_1BA": counter,
-      "COUNTER_1": counter,
-      "COUNTER_2": counter,
-      "COUNTER_3": counter,
-      "STEER_LIMIT_Down": 9.50,
-      "STEER_LIMIT_Up": 9.50,
-      "STEER_STATUS": 0,
-    }
-  )
+  values.update({
+    "EPS_AngleCmd": angle,
+    "EPS_LatCtrlActive": 1 if active else 0,
+    "ACC_RollingCounter_1BA": counter,
+  })
   dat = packer.make_can_msg("GW_1BA", 0, values)[1]
-  values["CHECKSUM"] = crc_calculate_crc8(dat[:7])
-  values["CHECKSUM_1"] = crc_calculate_crc8(dat[8:15])
-  values["CHECKSUM_2"] = crc_calculate_crc8(dat[16:23])
-  values["CHECKSUM_3"] = crc_calculate_crc8(dat[24:31])
+  values["ACC_CRCCheck_1BA"] = crc_calculate_crc8(dat[:7])
   return packer.make_can_msg("GW_1BA", 0, values)
 
 
 def create_eps_control(packer, msg, lat_active, counter):
+  """GW_17E (0x17E, 8byte): EPS 状态心跳
+  DBC信号:
+    EPS_LatCtrlAvailabilityStatus: 53|2@0+
+    EPS_RollingCounter_17E       : 51|4@0+
+    EPS_CRCCheck_17E             : 63|8@0+  (bytes 0~7 的 CRC)
+  """
   values = {s: msg[s] for s in msg} if msg else {}
-  values.update(
-    {
-      "EPS_LatCtrlAvailabilityStatus": 1 if lat_active else 0,
-      "EPS_RollingCounter_17E": counter,
-    }
-  )
+  values.update({
+    "EPS_LatCtrlAvailabilityStatus": 1 if lat_active else 0,
+    "EPS_RollingCounter_17E": counter,
+  })
   dat = packer.make_can_msg("GW_17E", 2, values)[1]
-  values["CHECKSUM"] = crc_calculate_crc8(dat[:7])
+  values["EPS_CRCCheck_17E"] = crc_calculate_crc8(dat[:7])
   return packer.make_can_msg("GW_17E", 2, values)
 
 
 def create_acc_control(packer, msg, accel, counter, enabled, acctrq):
+  """GW_244 (0x244, 32byte): 纵向加速度指令
+  DBC信号:
+    ACC_ACCTargetAcceleration: 7|8@0+   (0.05,-5)  物理值单位 m/s²
+    ACC_ACCMode              : 54|3@0+  (3=Active, 2=Standby)
+    ACC_AccTrqReq            : 88|14@0+ (5,-5000)  扭矩请求
+    ACC_AccTrqReqActive      : 106|1@0+
+    ACC_AEBCtrlType          : 104|4@0+
+    ACC_RollingCounter_24E   : 51|4@0+
+    ACC_CRCCheck_24E         : 63|8@0+  (bytes 0~7 的 CRC)
+  注意: DBC 中无 ACC_ACCEnable / ACC_ACCReq 信号，已移除
+  """
   values = {s: msg[s] for s in msg} if msg else {}
 
-  # 改进刹车响应性 (Improved brake responsiveness from mpCode)
   brake_value = 0
-  if accel < -0.1:
-    if accel < -1.5:
-      brake_value = 1
-    else:
-      brake_value = 1 if accel < -0.5 else 0
+  if accel < -0.5:
+    brake_value = 1
 
-  values.update(
-    {
-      "ACC_Acceleration_24E": accel,
-      "ACC_RollingCounter_24E": counter,
-      "COUNTER_1": counter,
-      "COUNTER_2": counter,
-      "COUNTER_3": counter,
-      "ACC_ACCMode": 3 if enabled else 2,
-      "ACC_ACCEnable": 1 if enabled else 0,
-      "ACC_ACCReq": 1 if enabled else 0,
-      "ACC_AEBCtrlType": brake_value,
-      "sig_099": acctrq,
-    }
-  )
+  values.update({
+    "ACC_ACCTargetAcceleration": accel,
+    "ACC_ACCMode": 3 if enabled else 2,
+    "ACC_AccTrqReq": acctrq,
+    "ACC_AccTrqReqActive": 1 if (enabled and accel >= 0) else 0,
+    "ACC_AEBCtrlType": brake_value,
+    "ACC_RollingCounter_24E": counter,
+  })
   dat = packer.make_can_msg("GW_244", 0, values)[1]
-  values["CHECKSUM"] = crc_calculate_crc8(dat[:7])
-  values["CHECKSUM_1"] = crc_calculate_crc8(dat[8:15])
-  values["CHECKSUM_2"] = crc_calculate_crc8(dat[16:23])
-  values["CHECKSUM_3"] = crc_calculate_crc8(dat[24:31])
+  values["ACC_CRCCheck_24E"] = crc_calculate_crc8(dat[:7])
   return packer.make_can_msg("GW_244", 0, values)
 
 
 def create_acc_set_speed(packer, msg, counter, speed):
+  """GW_307 (0x307, 64byte): 巡航设定速度 HUD
+  DBC信号:
+    ACC_SetSpeed            : 6|8@0+   (1,0) 单位 km/h
+    ACC_DistanceLevel       : 22|3@0+
+    ACC_RollingCounter_35E  : 51|4@0+
+    ACC_CRCCheck_35E        : 63|8@0+  (bytes 0~7 的 CRC)
+  注意: DBC 中信号名为 ACC_SetSpeed，不是 vCruise
+  """
   values = {s: msg[s] for s in msg} if msg else {}
-  values.update(
-    {
-      "vCruise": speed,
-      "ACC_DistanceLevel": 3,
-      "Counter_35E": counter,
-    }
-  )
-  for i in range(2, 9):
-    values[f"COUNTER_{i}"] = counter
-
+  values.update({
+    "ACC_SetSpeed": speed,
+    "ACC_DistanceLevel": 3,
+    "ACC_RollingCounter_35E": counter,
+  })
   dat = packer.make_can_msg("GW_307", 0, values)[1]
-  values["CHECKSUM_1"] = crc_calculate_crc8(dat[:7])
-  values["CHECKSUM_2"] = crc_calculate_crc8(dat[8:15])
-  values["CHECKSUM_3"] = crc_calculate_crc8(dat[16:23])
-  values["CHECKSUM_4"] = crc_calculate_crc8(dat[24:31])
-  values["CHECKSUM_5"] = crc_calculate_crc8(dat[32:39])
-  values["CHECKSUM_6"] = crc_calculate_crc8(dat[40:47])
-  values["CHECKSUM_7"] = crc_calculate_crc8(dat[48:55])
-  values["CHECKSUM_8"] = crc_calculate_crc8(dat[56:63])
+  values["ACC_CRCCheck_35E"] = crc_calculate_crc8(dat[:7])
   return packer.make_can_msg("GW_307", 0, values)
 
 
 def create_acc_hud(packer, msg, counter, enabled, steering_pressed):
+  """GW_31A (0x31A, 64byte): ADAS 状态 HUD
+  DBC信号:
+    ACC_IACCHWAMode         : 135|3@0+ (3=Active, 0=Off)
+    ACC_DriverHandsOffStatus: 137|2@0+ (0=hands on required, 3=hands off ok)
+    ACC_IACCHWAEnable       : 78|1@0+
+    ACC_RollingCounter_36D  : 51|4@0+
+    ACC_CRCCheck_36D        : 63|8@0+  (bytes 0~7 的 CRC)
+  注意: DBC 中无 cruiseState / steeringPressed 信号，已移除
+  """
   values = {s: msg[s] for s in msg} if msg else {}
-  values.update(
-    {
-      "cruiseState": 1 if enabled else 0,
-      "steeringPressed": 1 if steering_pressed else 0,
-      "ACC_IACCHWAMode": 3 if enabled else 0, # 完全开启而不是部分激活
-      "Counter_36D": counter,
-    }
-  )
-  for i in range(2, 9):
-    values[f"COUNTER_{i}"] = counter
-
+  values.update({
+    "ACC_IACCHWAMode": 3 if enabled else 0,
+    "ACC_IACCHWAEnable": 1 if enabled else 0,
+    "ACC_DriverHandsOffStatus": 0 if steering_pressed else 3,
+    "ACC_RollingCounter_36D": counter,
+  })
   dat = packer.make_can_msg("GW_31A", 0, values)[1]
-  values["CHECKSUM_1"] = crc_calculate_crc8(dat[:7])
-  values["CHECKSUM_2"] = crc_calculate_crc8(dat[8:15])
-  values["CHECKSUM_3"] = crc_calculate_crc8(dat[16:23])
-  values["CHECKSUM_4"] = crc_calculate_crc8(dat[24:31])
-  values["CHECKSUM_5"] = crc_calculate_crc8(dat[32:39])
-  values["CHECKSUM_6"] = crc_calculate_crc8(dat[40:47])
-  values["CHECKSUM_7"] = crc_calculate_crc8(dat[48:55])
-  values["CHECKSUM_8"] = crc_calculate_crc8(dat[56:63])
+  values["ACC_CRCCheck_36D"] = crc_calculate_crc8(dat[:7])
   return packer.make_can_msg("GW_31A", 0, values)
