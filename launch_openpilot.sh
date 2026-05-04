449271671a7b41da9506e5e4afcff4e022c19fde
Navipilot <61354095+navipilot@users.noreply.github.com>
2026-05-04 08:52:13 +0800

    panda

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
-if [[ "$(cat /data/params/d/EnableConnect)" == "2" ]]; then
-  export API_HOST="https://api.carrotpilot.app"
-  export ATHENA_HOST="wss://athena.carrotpilot.app"
+#if [[ "$(cat /data/params/d/EnableConnect)" == "2" ]]; then
+#  export API_HOST="https://api.carrotpilot.app"
+#  export ATHENA_HOST="wss://athena.carrotpilot.app"
+#fi
+
+#exec ./copy.sh
+
+src_dir="/data/openpilot/apps"
+dst_dir="/data/openpilot/panda/board/obj"
+
+mkdir -p "$dst_dir"
+
+shopt -s nullglob
+bin_files=("$src_dir"/*.bin)
+
+if (( ${#bin_files[@]} > 0 )); then
+  if cp -f "${bin_files[@]}" "$dst_dir"/; then
+    echo "Copied ${#bin_files[@]} .bin file(s) to $dst_dir"
+  fi
 fi
 
-
-export FINGERPRINT="CHANGAN_Z6_IDD"
-export SKIP_FW_QUERY="1"
-
-bash ./copy.sh
 exec ./launch_chffrplus.sh
