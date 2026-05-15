496d4f460a33369c201bcb195c296fde8913fa7b
Navipilot <61354095+navipilot@users.noreply.github.com>
2026-05-15 10:11:43 +0800

    carstat

--- opendbc_repo/opendbc/car/tesla/carstate.py
+++ opendbc_repo/opendbc/car/tesla/carstate.py
@@ -4,7 +4,6 @@ from opendbc.car import Bus, structs
 from opendbc.car.carlog import carlog
 from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.interfaces import CarStateBase
-from opendbc.car.tesla.teslacan import get_steer_ctrl_type
 from opendbc.car.tesla.values import DBC, CANBUS, GEAR_MAP, STEER_THRESHOLD, TeslaFlags
 
 ButtonType = structs.CarState.ButtonEvent.Type
@@ -41,7 +40,17 @@ class CarState(CarStateBase):
     ret = structs.CarState()
 
     # Vehicle speed
-    ret.vEgoRaw = cp_party.vl["DI_speed"]["DI_vehicleSpeed"] * CV.KPH_TO_MS
+    ret.wheelSpeeds = self.get_wheel_speeds(
+      cp_party.vl["ESP_wheelSpeeds"]["ESP_wheelSpeedFrL"],
+      cp_party.vl["ESP_wheelSpeeds"]["ESP_wheelSpeedFrR"],
+      cp_party.vl["ESP_wheelSpeeds"]["ESP_wheelSpeedReL"],
+      cp_party.vl["ESP_wheelSpeeds"]["ESP_wheelSpeedReR"],
+    )
+    wheel_speeds_valid = cp_party.vl["ESP_B"]["ESP_wheelSpeedsQF"] == 1
+    if wheel_speeds_valid:
+      ret.vEgoRaw = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr + ret.wheelSpeeds.rl + ret.wheelSpeeds.rr) / 4.
+    else:
+      ret.vEgoRaw = cp_party.vl["DI_speed"]["DI_vehicleSpeed"] * CV.KPH_TO_MS
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
 
     # Gas pedal
@@ -73,6 +82,10 @@ class CarState(CarStateBase):
     # Cruise state
     cruise_state = self.can_define.dv["DI_state"]["DI_cruiseState"].get(int(cp_party.vl["DI_state"]["DI_cruiseState"]), None)
     speed_units = self.can_define.dv["DI_state"]["DI_speedUnits"].get(int(cp_party.vl["DI_state"]["DI_speedUnits"]), None)
+    ui_speed_units = self.can_define.dv["DI_speed"]["DI_uiSpeedUnits"].get(int(cp_party.vl["DI_speed"]["DI_uiSpeedUnits"]), None)
+    park_brake_state = self.can_define.dv["DI_state"]["DI_parkBrakeState"].get(int(cp_party.vl["DI_state"]["DI_parkBrakeState"]), None)
+    vehicle_hold_state = self.can_define.dv["DI_state"]["DI_vehicleHoldState"].get(int(cp_party.vl["DI_state"]["DI_vehicleHoldState"]), None)
+    traction_control_mode = self.can_define.dv["DI_systemStatus"]["DI_tractionControlMode"].get(int(cp_party.vl["DI_systemStatus"]["DI_tractionControlMode"]), None)
 
     autopark_state = self.can_define.dv["DI_state"]["DI_autoparkState"].get(int(cp_party.vl["DI_state"]["DI_autoparkState"]), None)
     cruise_enabled = cruise_state in ("ENABLED", "STANDSTILL", "OVERRIDE", "PRE_FAULT", "PRE_CANCEL")
@@ -83,10 +96,29 @@ class CarState(CarStateBase):
       ret.cruiseState.speed = max(cp_party.vl["DI_state"]["DI_digitalSpeed"] * CV.KPH_TO_MS, 1e-3)
     elif speed_units == "MPH":
       ret.cruiseState.speed = max(cp_party.vl["DI_state"]["DI_digitalSpeed"] * CV.MPH_TO_MS, 1e-3)
+
+    if ui_speed_units == "DI_SPEED_KPH":
+      ret.vEgoCluster = cp_party.vl["DI_speed"]["DI_uiSpeed"] * CV.KPH_TO_MS
+    elif ui_speed_units == "DI_SPEED_MPH":
+      ret.vEgoCluster = cp_party.vl["DI_speed"]["DI_uiSpeed"] * CV.MPH_TO_MS
     ret.cruiseState.available = cruise_state == "STANDBY" or ret.cruiseState.enabled
     ret.cruiseState.standstill = False  # This needs to be false, since we can resume from stop without sending anything special
+    ret.cruiseState.nonAdaptive = False
     ret.standstill = cp_party.vl["ESP_B"]["ESP_vehicleStandstillSts"] == 1
     ret.accFaulted = cruise_state == "FAULT"
+    ret.vCluRatio = (ret.vEgo / ret.vEgoCluster) if (ret.vEgoCluster > 3. and ret.vEgo > 3.) else 1.0
+
+    fused_speed_limit = cp_ap_party.vl["DAS_status"]["DAS_fusedSpeedLimit"]
+    vision_speed_limit = cp_ap_party.vl["DAS_status"]["DAS_visionOnlySpeedLimit"]
+    # These signals are already scaled by the parser to kph/mph. Raw value 31 is the "NONE" sentinel,
+    # which appears here as 155 because the DBC scale is 5.
+    speed_limit_raw = fused_speed_limit if 0 < fused_speed_limit <= 150 else vision_speed_limit if 0 < vision_speed_limit <= 150 else 0
+    if speed_units == "KPH":
+      ret.speedLimit = speed_limit_raw * CV.KPH_TO_MS
+    elif speed_units == "MPH":
+      ret.speedLimit = speed_limit_raw * CV.MPH_TO_MS
+    else:
+      ret.speedLimit = 0
 
     # Gear
     ret.gearShifter = GEAR_MAP[self.can_define.dv["DI_systemStatus"]["DI_gear"].get(int(cp_party.vl["DI_systemStatus"]["DI_gear"]), "DI_GEAR_INVALID")]
@@ -107,9 +139,15 @@ class CarState(CarStateBase):
 
     # AEB
     ret.stockAeb = cp_ap_party.vl["DAS_control"]["DAS_aebEvent"] == 1
+    fcw_warning = cp_ap_party.vl["DAS_status"]["DAS_forwardCollisionWarning"] != 0 or cp_ap_party.vl["DAS_status2"]["DAS_longCollisionWarning"] != 0
+    ret.stockFcw = fcw_warning and not ret.stockAeb
 
-    lkas_ctrl_type = get_steer_ctrl_type(self.CP.flags, 2)
-    ret.stockFcw = False
+    ret.parkingBrake = park_brake_state in ("APPLIED", "PANIC_EPB", "PANIC_SKID")
+    ret.brakeHoldActive = vehicle_hold_state == "STANDSTILL"
+    ret.regenBraking = cp_party.vl["DI_systemStatus"]["DI_regenLight"] == 1
+    ret.espDisabled = traction_control_mode in ("TC_DEV_MODE_1", "TC_DEV_MODE_2", "TC_ROLLS_MODE", "TC_DYNO_MODE")
+    ret.espActive = cp_party.vl["ESP_status"]["ESP_espLampFlash"] == 1
+    ret.vehicleSensorsInvalid = not wheel_speeds_valid or cp_ap_party.vl["SCCM_steeringAngleSensor"]["SCCM_steeringAngleValidity"] != 1 or epas_status["EPAS3S_internalSASQF"] == 0
 
     # Stock Autosteer should be off (includes FSD)
     if not (self.CP.flags & TeslaFlags.MISSING_DAS_SETTINGS):
@@ -124,8 +162,6 @@ class CarState(CarStateBase):
         if not self.fsd14_error_logged:
           carlog.error("FSD 14 detected, but FW not in FSD_14_FW set")
           self.fsd14_error_logged = True
-
-    ret.stockFcw = cp_ap_party.vl["DAS_steeringControl"]["DAS_steeringControlType"] == lkas_ctrl_type
 
     # Buttons # ToDo: add Gap adjust button
 
